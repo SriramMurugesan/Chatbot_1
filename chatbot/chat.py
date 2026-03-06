220bdd7d57395e8f97b116f24d2b362838981d81
SriramMurugesan <sri2014ram@gmail.com>
2026-03-06 12:11:06 +0530

    Adsd

---
+++ chatbot/chat.py
@@ -0,0 +1,20 @@
+from google import genai
+
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+
+
+
+client = genai.Client()
+
+response = client.models.generate_content(
+    model="gemini-2.5-flash",
+
+    contents="who is Gokulkrishnan muthusamy from iitm bs degree?",
+)
+
+print(response.text)
